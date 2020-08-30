36f8d615c14616af08583b2099f4c967a871de36
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2020-08-30 16:27:49 +0300

    exercise 3

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ app.get('/api/info', (req, res) => {
   res.send(p + " " + date)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+})
+
 const PORT = 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
