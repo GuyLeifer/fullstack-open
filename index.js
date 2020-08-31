2e4e4572c64f51baf111c36d0e9f77c7150eebe6
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2020-08-31 15:41:19 +0300

    heroku

--- index.js
+++ index.js
@@ -76,9 +76,8 @@ app.get('/api/persons/:id', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
-
   response.send(persons)
-  response.status(204).end()
+  // response.status(204).end()
 })
 
 const generateId = () => {
